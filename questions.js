5d6e3a1db938afe679955697f8512c39776d1357
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-27 22:43:08 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,23 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Don't expect someone to pick it ___ for you.",
+        options:["up", "over", "out", "for", "after"],
+        answer: 0
+    },
+    {
+        q:"He lost his balance and fell ___ .",
+        options:["up", "out", "for", "after", "over"],
+        answer: 4
+    },
+    {
+        q:"I give ___ , I can't do this.",
+        options:["down", "out", "about", "at", "up"],
+        answer: 4
+    },
+    {
+        q:"Can I write ___ my address and phone number for you?",
+        options:["at", "up", "down", "about", "out"],
+        answer: 2
+    }
+]
